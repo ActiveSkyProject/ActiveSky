6325279275015890b571ac10622c271771a40db5
Elif Tufekci <wwweliftufekci@gmail.com>
2025-03-21 18:09:02 +0300

    OpenWeatherApi doğru branche eklendi

--- website/views.py
+++ website/views.py
@@ -15,3 +15,25 @@ def home():
 @login_required
 def mainPage():
     return render_template("anasayfa.html", user=current_user)
+
+@views.route('/anasayfa', methods=['POST'])
+@login_required
+def get_weather():
+    data = request.get_json()
+    latitude = data['latitude']
+    longitude = data['longitude']
+
+    API_KEY = get_api_key()
+    url = f'http://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&units=metric&appid={API_KEY}'
+
+    response = requests.get(url).json()
+    city = response['name']
+    temp = response['main']['temp']
+    description = response['weather'][0]['description']
+
+    return jsonify({'city': city, 'temp': temp, 'description': description})
+
+def get_api_key():
+    file_path = "C:\\Users\\elif4\\ActiveSky\\api_key_txt"
+    with open(file_path, 'r') as file:
+        return file.read().strip()
